579d2a853b360e96e1cdb0e1f7fcdabee2b430aa
almusto <ales.musto@gmail.com>
2017-01-30 09:47:02 -0500

    learnin

--- ViewLifeCycle/ViewController.swift
+++ ViewLifeCycle/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad() called")
+        print("viewDidLoad() was called")
         view.backgroundColor = UIColor.randomColor()
     }
     
